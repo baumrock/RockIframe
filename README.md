9b6f155a7dbe95ad65d6ec6f7fcbf1a9e474f211
BernhardBaumrock <office@baumrock.com>
2021-03-03 20:26:21 +0100

    first version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # RockIframe
+
+Iframe Sidebar for the ProcessWire page edit screen
+
+## Problem
+
+Sometimes the editor wants to see a preview of a file while editing a page. For example this can be helpful when digitizing invoices. Regular ProcessWire panels can be used for preview but overlap the page editor so the user has to open and close the panel repeatedly while working.
+
+RockIframe shows a preview of any content that can be rendered in an iframe and keeps the page editor 100% visible to the user.
+
+## Usage
+
+Simply call `$modules->get('RockIframe')->show('path/to/your/file.pdf')` anywhere in the admin to show your data in a sidebar iframe on page load. You can also define a Pagefile(s) object as source of the iframe.
+
+## Example
+
+This example shows the first file of the field `myfilesfield` on the page edit screen of pages having template `mypagetemplate`:
+
+```php
+$wire->addHookAfter("ProcessPageEdit::buildForm", function($event) {
+  $page = $event->process->getPage();
+  if($page->template !== 'mypagetemplate') return;
+
+  /** @var RockIframe $iframe */
+  $iframe = $this->wire->modules->get('RockIframe');
+  if($iframe) $iframe->show($page->get('myfilesfield'));
+});
+```
+
+![img](https://i.imgur.com/9e5KvTY.png)
+
+## Ideas & Notes
+
+PDF preview relies on the browser's capability to preview PDFs. Also at the moment there are no checks which content is thrown to the iframe as source. For images for example we could use https://leafletjs.com/examples/crs-simple/crs-simple.html to enable panning & zooming on the image. For better browsersupport of PDF we could use https://mozilla.github.io/pdf.js/web/viewer.html;
---
+++ RockIframe.module.php
@@ -0,0 +1,65 @@
+<?php namespace ProcessWire;
+/**
+ * Iframe Sidebar for the ProcessWire page edit screen
+ *
+ * @author Bernhard Baumrock, 03.03.2021
+ * @license Licensed under MIT
+ * @link https://www.baumrock.com
+ */
+class RockIframe extends WireData implements Module {
+  private $frame;
+
+  public static function getModuleInfo() {
+    return [
+      'title' => 'RockIframe',
+      'version' => '1.0.0',
+      'summary' => 'Iframe Sidebar for the ProcessWire page edit screen',
+      // must be true, not template=admin!
+      // otherwise the pageview hook will not fire
+      'autoload' => true,
+      'singular' => true,
+      'icon' => 'columns',
+      'requires' => [],
+      'installs' => [],
+    ];
+  }
+
+  public function init() {
+    $this->addHookAfter("ProcessPageView::execute", $this, "addIframe");
+    $url = $this->config->urls($this)."RockIframe.css";
+    $this->style = "<link type='text/css' href='$url' rel='stylesheet'>";
+  }
+
+  public function addIframe(HookEvent $event) {
+    if(!$this->frame) return;
+    $html = str_replace("</body>", $this->frame."</body>", $event->return);
+    $html = str_replace("</head>", $this->style."</head>", $html);
+    $event->return = $html;
+  }
+
+  public function show($data) {
+    $url = $this->getUrl($data);
+    if(!$url) return;
+    $this->frame = "<iframe src='$url' class='RockIframe'></iframe>";
+  }
+
+  public function getUrl($data) {
+    $config = $this->wire->config;
+    if(is_file($data)) {
+      $url = str_replace($config->paths->root, $config->urls->root, $data);
+      return $url."?m=".filemtime($data);
+    }
+    if($data instanceof Pagefile) {
+      $file = $data;
+      if(!$file->filemtime) return;
+      return $file->url."?m=".$file->filemtime;
+    }
+    if($data instanceof Pagefiles) {
+      if(!$data->count()) return;
+      $file = $data->first();
+      if(!$file->filemtime) return;
+      return $file->url."?m=".$file->filemtime;
+    }
+    return $data;
+  }
+}
