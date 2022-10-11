92ab5ae2c394ded50b505008f422b72945eefc9b
BernhardBaumrock <office@baumrock.com>
2022-10-11 10:50:51 +0200

    improve image rendering

--- RockIframe.module.php
+++ RockIframe.module.php
@@ -1,4 +1,7 @@
-<?php namespace ProcessWire;
+<?php
+
+namespace ProcessWire;
+
 /**
  * Iframe Sidebar for the ProcessWire page edit screen
  *
@@ -6,10 +9,12 @@
  * @license Licensed under MIT
  * @link https://www.baumrock.com
  */
-class RockIframe extends WireData implements Module {
+class RockIframe extends WireData implements Module
+{
   private $frame;
 
-  public static function getModuleInfo() {
+  public static function getModuleInfo()
+  {
     return [
       'title' => 'RockIframe',
       'version' => '1.0.4',
@@ -24,37 +29,41 @@ class RockIframe extends WireData implements Module {
     ];
   }
 
-  public function init() {
-    $url = $this->config->urls($this)."RockIframe.css";
+  public function init()
+  {
+    $url = $this->config->urls($this) . "RockIframe.css";
     $this->style = "<link type='text/css' href='$url' rel='stylesheet'>";
 
     $this->addHookAfter("ProcessPageView::execute", $this, "addIframe");
     $this->addHookAfter("/rockiframeleaflet/", $this, "renderLeaflet");
+    $this->addHookAfter("/rockiframeimage/", $this, "renderImage");
   }
 
-  public function addIframe(HookEvent $event) {
-    if(!$this->frame) return;
-    $html = str_replace("</body>", $this->frame."</body>", $event->return);
-    $html = str_replace("</head>", $this->style."</head>", $html);
+  public function addIframe(HookEvent $event)
+  {
+    if (!$this->frame) return;
+    $html = str_replace("</body>", $this->frame . "</body>", $event->return);
+    $html = str_replace("</head>", $this->style . "</head>", $html);
     $event->return = $html;
   }
 
-  public function getUrl($data) {
+  public function getUrl($data)
+  {
     $config = $this->wire->config;
-    if($this->wire->files->fileInPath($data, $config->paths->root) AND is_file($data)) {
+    if ($this->wire->files->fileInPath($data, $config->paths->root) and is_file($data)) {
       $url = str_replace($config->paths->root, $config->urls->root, $data);
-      return $url."?m=".filemtime($data);
+      return $url . "?m=" . filemtime($data);
     }
-    if($data instanceof Pagefile) {
+    if ($data instanceof Pagefile) {
       $file = $data;
-      if(!$file->filemtime) return;
-      return $file->url."?m=".$file->filemtime;
+      if (!$file->filemtime) return;
+      return $file->url . "?m=" . $file->filemtime;
     }
-    if($data instanceof Pagefiles) {
-      if(!$data->count()) return;
+    if ($data instanceof Pagefiles) {
+      if (!$data->count()) return;
       $file = $data->first();
-      if(!$file->filemtime) return;
-      return $file->url."?m=".$file->filemtime;
+      if (!$file->filemtime) return;
+      return $file->url . "?m=" . $file->filemtime;
     }
     return $data;
   }
@@ -63,8 +72,10 @@ class RockIframe extends WireData implements Module {
    * Show image file in a leaflet map viewer
    * @return void
    */
-  public function leaflet($img, $options = []) {
-    $opt = $this->wire(new WireData()); /** @var WireData $opt */
+  public function leaflet($img, $options = [])
+  {
+    $opt = $this->wire(new WireData());
+    /** @var WireData $opt */
     $opt->setArray([
       'x' => 1000,
       'y' => 1000,
@@ -75,7 +86,7 @@ class RockIframe extends WireData implements Module {
     $opt->setArray($options);
 
     $optstr = '';
-    foreach($opt->getArray() as $k=>$v) $optstr .= "&$k=$v";
+    foreach ($opt->getArray() as $k => $v) $optstr .= "&$k=$v";
 
     $config = $this->wire->config;
     $img = str_replace($config->paths->root, $config->urls->root, $img);
@@ -83,10 +94,25 @@ class RockIframe extends WireData implements Module {
   }
 
   /**
+   * Show image file in iframe
+   * @return void
+   */
+  public function image($img, $options = [])
+  {
+    if ($img instanceof Pagefiles) $img = $img->first();
+    if (!$img instanceof Pagefile) return false;
+    $images = ['jpg', 'jpeg', 'gif', 'png', 'webp'];
+    if (in_array($img->ext, $images)) {
+      $this->frame = "<iframe src='/rockiframeimage/?img={$img->url}' class='RockIframe'></iframe>";
+    } else $this->show($img);
+  }
+
+  /**
    * Render the leaflet viewer
    * @return string
    */
-  public function renderLeaflet(HookEvent $event) {
+  public function renderLeaflet(HookEvent $event)
+  {
     // add a runtime flag to the page object
     // this is for apps where all frontend requests are redirected to the backend
     // checking for this page property makes it possible to prevent redirect
@@ -95,11 +121,11 @@ class RockIframe extends WireData implements Module {
     $page->title = 'RockIframe Leaflet Viewer';
 
     $img = $this->wire->input->get('img', 'string');
-    $file = $this->wire->config->paths->root.ltrim($img, "/");
-    if(is_file($file)) $img .= "?m=".filemtime($file);
+    $file = $this->wire->config->paths->root . ltrim($img, "/");
+    if (is_file($file)) $img .= "?m=" . filemtime($file);
 
     $path = $this->wire->config->paths($this);
-    return $this->wire->files->render($path."leaflet/viewer.php", [
+    return $this->wire->files->render($path . "leaflet/viewer.php", [
       'img' => $img,
       'x' => $this->wire->input->get('x', 'int'),
       'y' => $this->wire->input->get('y', 'int'),
@@ -109,14 +135,26 @@ class RockIframe extends WireData implements Module {
     ]);
   }
 
-  public function show($data) {
+  public function renderImage(HookEvent $event)
+  {
+    $img = $this->wire->input->get('img', 'string');
+    return "<html><head><style>
+      img{max-width:100%}
+      html,body{padding:0;margin:0;}
+      </style><body>
+      <img src=$img>
+      </body></html>";
+  }
+
+  public function show($data)
+  {
     $url = $this->getUrl($data);
-    if(!$url) return;
+    if (!$url) return;
     $this->frame = "<iframe src='$url' class='RockIframe'></iframe>";
   }
 
-  public function showUrl($url) {
+  public function showUrl($url)
+  {
     $this->frame = "<iframe src='$url' class='RockIframe'></iframe>";
   }
-
 }
