9ccaa2a49f5798d5efdee69f700ccc5e2b7a83df
BernhardBaumrock <office@baumrock.com>
2023-05-09 20:33:09 +0200

    feat: remember center + zoom

--- leaflet/viewer.php
+++ leaflet/viewer.php
@@ -1,23 +1,26 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
   <title><?= $page->title ?></title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
   <style>
-    html, body {
+    html,
+    body {
       height: 100%;
       margin: 0;
     }
+
     #map {
       width: 100%;
       height: 100%;
     }
   </style>
 </head>
+
 <body>
   <div id='map'></div>
   <script>
@@ -26,10 +29,30 @@
       minZoom: <?= $minZoom ?>,
       maxZoom: <?= $maxZoom ?>
     });
-    var bounds = [[0,0], [<?= $y ?>,<?= $x ?>]];
+    var bounds = [
+      [0, 0],
+      [<?= $y ?>, <?= $x ?>]
+    ];
     var image = L.imageOverlay('<?= $img ?>', bounds).addTo(map);
     map.fitBounds(bounds);
-    <?php if(isset($zoom)) echo "map.setZoom($zoom);"; ?>
+    let flyto = localStorage.getItem('map-flyto');
+    if (flyto) {
+      flyto = JSON.parse(flyto);
+      // map.flyTo([flyto.lat, flyto.lng], flyto.zoom);
+      map.setView(new L.LatLng(flyto.lat, flyto.lng), flyto.zoom);
+    }
+    <?php if (isset($zoom)) echo "if(!flyto) map.setZoom($zoom);"; ?>
+    map.on('moveend', (e) => {
+      let center = map.getCenter();
+      let json = JSON.stringify({
+        lat: center.lat,
+        lng: center.lng,
+        zoom: map.getZoom(),
+      });
+      console.log(json);
+      localStorage.setItem('map-flyto', json);
+    });
   </script>
 </body>
+
 </html>
